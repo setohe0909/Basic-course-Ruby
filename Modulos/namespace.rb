e6faea9fad0962608e2310aee60923043416d783
seto <zero@seto-zero.(none)>
2015-04-22 16:44:48 -0500

    modificacion modulos

--- Modulos/namespace.rb
+++ Modulos/namespace.rb
@@ -19,3 +19,43 @@
 	    # Invocación:
 	    	cita_1 = Inspiracion::Cita.new
 	    	cita_2 = Legal::Cita.new
+
+# Módulo mixin
+	module InformacionContacto
+	  attr_accessor :primer_nombre, :primer_apellido, :ciudad, :estado, :codigo_zip
+
+	  def nombre_completo
+	    return @primer_nombre + " " + @primer_apellido
+	  end
+
+	  def ciudad_estado_zip
+	    cez = @ciudad
+	    cez += "* #{@estado}" if @estado
+	    cez += "  #{@codigo_zip}" if @codigo_zip
+	    return cez
+	  end
+	end
+
+	class Persona
+	 include InformacionContacto
+	end
+
+	class Profesor
+	  include InformacionContacto
+	  attr_accessor :plan_de_estudio
+	end
+
+	class Estudiante < Persona
+	  attr_accessor :horario
+	end
+
+# Comandos load, requires e include
+	# Con los siguientes comando se pueden  optimizar el consumo de los modulos
+		# load => se usa para cargar el codigo tantas veces sea necesario
+			# Se inicia al principio del archivo lo siguiente:
+			load 'modulo.rb' 
+		# requiere => Solo carga una vez el metodo que estamos utilizando
+			requiere 'modulo.rb' 
+		# include => Permite incluir el uso de un modulo directamente en una
+		# unica clase
+			include InformacionContacto
