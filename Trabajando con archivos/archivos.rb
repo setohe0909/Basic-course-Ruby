70e3dc687d48f8d4b291457ca4226c7839bd5d73
setohe0909 <setohe.09@gmail.com>
2015-04-23 10:30:42 -0500

    trabajando con archivos init

--- Trabajando con archivos/archivos.rb
+++ Trabajando con archivos/archivos.rb
@@ -1,1 +1,25 @@
 # Trabajando con archivos
+	# Elementos de entrada y salida básicos
+	# Ejemplo:
+	#
+	# gets => Permite obtener información
+
+	#capturamos el valor agregado a get y junto con chomp podemos eliminar el santo de linea \n
+	# o cualquier tipo de formato que pueda existir
+		input = gets.chomp
+
+	# Ejemplo chomp, donde eliminara la ultima letra del texto
+		"Ultimo elemento r".chomp
+			result => "Ultimo elemento"
+
+	# Los siguientes son metodos de salida
+		print "Acabas de escribir: "
+		puts input + "."
+
+	resultado = ""
+	until resultado == "terminar"
+	  print "> "
+	  resultado = gets.chomp
+	  puts "Escuchando: #{resultado}"
+	end
+	puts "Hasta pronto"
